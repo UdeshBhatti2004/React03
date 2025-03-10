bc2e27df5f9f71eb60177e846c1dc7bb012c662f
UdeshBhatti2004 <164016941+UdeshBhatti2004@users.noreply.github.com>
2025-03-10 12:39:57 +0530

    Update Finance.jsx

--- src/Components/Finance.jsx
+++ src/Components/Finance.jsx
@@ -1,24 +1,21 @@
-
 import React from 'react'
 
 function Finance() {
   return (
     <>
-      <div className="main h-auto  w-full ">
-
-      
-         <div className="image  flex justify-center"> 
+      <div className="main h-auto w-full">
+        
+        <div className="image flex justify-center"> 
           <img
             src="/images/homeServiceimg.jpg"
-            className="h-[100vh] w-full object-contain mt-5" 
+            className="h-[50vh] sm:h-[60vh] lg:h-[100vh] w-full object-cover mt-5" 
             alt="Home Service"
           />
         </div>
 
- 
-        <div className="elements grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 px-[80px] mt-24">
+        <div className="elements grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 px-4 sm:px-8 lg:px-[80px] mt-12 sm:mt-16 lg:mt-24">
           
-          <div className="element-1 flex flex-col items-center text-center ">
+          <div className="element-1 flex flex-col items-center text-center">
             <div className="image">
               <img
                 src="/images/finance 1.png"
@@ -42,7 +39,6 @@ function Finance() {
             <h1 className="text-black mt-5 roboto-extra-light text-xl w-[150px]">Customer Satisfection</h1>
           </div>
 
-          
           <div className="element-3 flex flex-col items-center text-center">
             <div className="image">
               <img
@@ -52,12 +48,9 @@ function Finance() {
               />
             </div>
             <h1 className="text-[rgb(161,158,153)] text-4xl roboto-thin mt-5">70+</h1>
-            <h1 className="text-black mt-5  roboto-extra-light text-xl w-[200px]">
-              Bank / NBFC's
-            </h1>
+            <h1 className="text-black mt-5 roboto-extra-light text-xl w-[200px]">Bank / NBFC's</h1>
           </div>
 
-   
           <div className="element-4 flex flex-col items-center text-center">
             <div className="image">
               <img
@@ -76,5 +69,3 @@ function Finance() {
 }
 
 export default Finance
-
-
