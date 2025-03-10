ddbd78080551b6ec21e879f73f1d66e6a7ce9bfc
UdeshBhatti2004 <164016941+UdeshBhatti2004@users.noreply.github.com>
2025-03-10 12:39:09 +0530

    Update ChooseUs.jsx

--- src/Components/ChooseUs.jsx
+++ src/Components/ChooseUs.jsx
@@ -3,30 +3,24 @@ import React from "react";
 function ChooseUs() {
   return (
     <div className="main mt-12">
-    
       <div className="background-image bg-[url('/images/HomeAbout.png')] bg-cover bg-center w-full h-auto">
-   
         <div className="heading flex justify-center">
           <h1 className="text-white roboto-light text-4xl sm:text-5xl mt-8 text-center">
             Why Choose Us
           </h1>
         </div>
 
- 
         <div className="parts grid grid-cols-1 lg:grid-cols-1 xl:grid-cols-2 gap-8 items-center mt-4 px-4 lg:px-16">
-        
           <div className="image w-full">
             <img
-              className="object-contain h-[120vh] w-full"
+              className="object-cover h-auto sm:h-[50vh] lg:h-[120vh] w-full"
               src="/images/HomeAboutimg.png"
               alt="Why Choose Us"
             />
           </div>
 
-     
           <div className="content w-full text-white roboto-thin space-y-8">
-           
-            <div className="intro ">
+            <div className="intro">
               <p>
                 At Manba Finanserve, we are more than a loan services provider—
                 we are your gateway to financial confidence. When you choose us,
@@ -39,7 +33,6 @@ function ChooseUs() {
               </p>
             </div>
 
-          
             <div className="feature flex items-center gap-4">
               <img
                 className="h-20 w-20 object-contain"
@@ -55,7 +48,6 @@ function ChooseUs() {
               </div>
             </div>
 
-            
             <div className="feature flex items-center gap-4">
               <img
                 className="h-20 w-20 object-contain"
@@ -63,9 +55,7 @@ function ChooseUs() {
                 alt="Comprehensive Loan Options"
               />
               <div>
-                <h2 className="text-xl roboto-light">
-                  Comprehensive Loan Options
-                </h2>
+                <h2 className="text-xl roboto-light">Comprehensive Loan Options</h2>
                 <p className="text-sm">
                   We offer a diverse range of loan products to meet every need,
                   including personal, gold, home, and other loans designed just
@@ -74,8 +64,7 @@ function ChooseUs() {
               </div>
             </div>
 
-         
-            <div className="feature flex items-center gap-4 ">
+            <div className="feature flex items-center gap-4">
               <img
                 className="h-20 w-20 object-contain"
                 src="/images/aboutimg3.png"
@@ -97,3 +86,4 @@ function ChooseUs() {
 }
 
 export default ChooseUs;
+
